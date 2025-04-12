163bf2a879b20320380b9a082bbd099845e048ef
siraajul <shirajulislamparvez@gmail.com>
2025-04-12 16:28:46 +0600

    Get Title Automation

--- src/test/java/DemoQA.java
+++ src/test/java/DemoQA.java
@@ -1,15 +1,30 @@
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DemoQA {
+
+    WebDriver driver;
+
+    @BeforeAll
     public void setup() {
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();//maximize window
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     }
-    public void visitUrl(){
 
+    @Test
+    public void visitUrl() {
+        driver.get("https://demoqa.com/");
+        String titleActual = driver.getTitle();
+        String titleExpected = "DEMOQA";
+        Assertions.assertTrue(titleActual.equals(titleExpected));
+    }
+    @AfterAll
+    public void tearDown(){
+        driver.quit();
     }
 }
