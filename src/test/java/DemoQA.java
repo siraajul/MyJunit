95a9a0a95863c5b30c15fe2544f01790976b9dd9
siraajul <shirajulislamparvez@gmail.com>
2025-04-13 20:21:55 +0600

    Form Manipulation

--- src/test/java/DemoQA.java
+++ src/test/java/DemoQA.java
@@ -1,8 +1,12 @@
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DemoQA {
@@ -16,14 +20,32 @@ public class DemoQA {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     }
 
-    @Test
+    //@Test
     public void visitUrl() {
         driver.get("https://demoqa.com/");
         String titleActual = driver.getTitle();
         String titleExpected = "DEMOQA";
         Assertions.assertTrue(titleActual.equals(titleExpected));
     }
-    @AfterAll
+
+    @Test
+    public void fillupUserForm(){
+        driver.get("https://demoqa.com/text-box");
+        //driver.findElement(By.id("userName")).sendKeys("Test User");
+        //driver.findElement(By.cssSelector("[type=text]")).sendKeys("Test User");
+        List<WebElement> txtFields = driver.findElements(By.className("form-control"));
+        txtFields.get(0).sendKeys("Test User");
+        txtFields.get(1).sendKeys("testUser@test.com");
+        txtFields.get(2).sendKeys("Gulshan");
+        txtFields.get(3).sendKeys("Dhaka");
+        //Scroll Down
+        JavascriptExecutor js =(JavascriptExecutor)driver;
+        js.executeScript("window.scrollBy(0,250)","");
+        //Button Submit
+        List<WebElement> btnSubmit = driver.findElements(By.tagName("button"));
+        btnSubmit.get(1).click();
+    }
+    //@AfterAll
     public void tearDown(){
         driver.quit();
     }
